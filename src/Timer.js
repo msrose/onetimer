b1724f3c9d0feceb64cf5e0e8743a790e026149a
Michael Rose <michael_rose@outlook.com>
2017-08-21 20:53:53 -0400

    Add menu to switch puzzles

--- src/Timer.js
+++ src/Timer.js
@@ -1,12 +1,50 @@
 import React from 'react';
+import Button from 'material-ui/Button';
 import './Timer.css';
+import { connect } from 'react-redux';
+import SwapIcon from 'material-ui-icons/SwapVert';
+import Menu, { MenuItem } from 'material-ui/Menu';
 
-const Timer = () => (
+const puzzles = [
+  '2x2x2', '3x3x3', '4x4x4', '5x5x5', '6x6x6', '7x7x7'
+];
+
+const PuzzleMenuItem = ({ onClick, name, selected }) => (
+  <MenuItem onClick={() => onClick(name)} selected={selected}>{name}</MenuItem>
+);
+
+const Timer = ({ puzzle, onSwitchPuzzle, onChangePuzzle, isSwitchPuzzleOpen }) => (
   <div className="Timer">
     <div className="Timer-display">
       00:00.000
     </div>
+    <div className="Timer-puzzle">
+      {puzzle}
+    </div>
+    <div className="Timer-swap">
+      <Button fab={true} onClick={onSwitchPuzzle}><SwapIcon /></Button>
+    </div>
+    <Menu open={isSwitchPuzzleOpen} onRequestClose={onSwitchPuzzle}>
+      {puzzles.map(name => (
+        <PuzzleMenuItem onClick={onChangePuzzle} name={name} selected={name === puzzle} key={name} />
+      ))}
+    </Menu>
   </div>
 );
 
-export default Timer;
+const mapStateToProps = state => {
+  return {
+    puzzle: state.timer.puzzle,
+    isSwitchPuzzleOpen: state.ui.isSwitchPuzzleOpen
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  onSwitchPuzzle: () => dispatch({ type: 'TOGGLE_SWITCH_PUZZLE' }),
+  onChangePuzzle: puzzle => {
+    dispatch({ type: 'SET_PUZZLE', puzzle });
+    dispatch({ type: 'TOGGLE_SWITCH_PUZZLE' });
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Timer);
--- src/reducers.js
+++ src/reducers.js
@@ -1,16 +1,32 @@
 import { combineReducers } from 'redux';
 
 const initialUIState = {
-  isDrawerOpen: false
+  isDrawerOpen: false,
+  isSwitchPuzzleOpen: false
 };
 
 function ui(state = initialUIState, action) {
   switch(action.type) {
     case 'TOGGLE_DRAWER':
       return { ...state, isDrawerOpen: !state.isDrawerOpen };
+    case 'TOGGLE_SWITCH_PUZZLE':
+      return { ...state, isSwitchPuzzleOpen: !state.isSwitchPuzzleOpen };
     default:
       return state;
   }
 }
 
-export default combineReducers({ ui });
+const initialTimerState = {
+  puzzle: '3x3x3'
+};
+
+function timer(state = initialTimerState, action) {
+  switch(action.type) {
+    case 'SET_PUZZLE':
+      return { ...state, puzzle: action.puzzle };
+    default:
+      return state;
+  }
+}
+
+export default combineReducers({ ui, timer });
