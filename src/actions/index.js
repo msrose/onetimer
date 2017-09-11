8aeceaa6d0f170599534173794ce101571db0342
Michael Rose <michael_rose@outlook.com>
2017-09-10 22:43:14 -0400

    Remove everything to prep for GitHub pages

--- src/actions/index.js
+++
@@ -1,5 +0,0 @@
-// Export everything here so import paths are easy
-export * from './timer';
-export * from './solves';
-export * from './ui';
-export * from './puzzles';
--- src/setupTests.js
+++
@@ -1,7 +0,0 @@
-const localStorageMock = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  clear: jest.fn()
-};
-
-global.localStorage = localStorageMock;
