1f6c7485f5448b2d1e21889deadadc5d818d2efb
Michael Rose <michael_rose@outlook.com>
2017-09-24 16:47:51 -0400

    Add empty state for solve list

--- src/components/Solves.js
+++ src/components/Solves.js
@@ -1,33 +1,40 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import AppBarMargin from './AppBarMargin';
-import { getActivePuzzleSolves, getHasActiveSelectedSolves } from '../reducers';
+import { getActivePuzzleSolves, getHasActiveSelectedSolves, getActivePuzzle } from '../reducers';
 import List from 'material-ui/List';
 import { toggleSolveSelected } from '../actions';
 import DeletedSolveMessage from './DeletedSolveMessage';
 import Solve from './Solve';
+import './Solves.css';
 
-const Solves = ({ solves, onSolveClick, showCheckboxes }) => (
-  <AppBarMargin>
-    <List>
-      {solves.map(({ duration, recordedAt, selected }) => (
-        <Solve
-          key={recordedAt}
-          recordedAt={recordedAt}
-          duration={duration}
-          selected={selected}
-          onClick={onSolveClick}
-          showCheckbox={showCheckboxes}
-        />
-      ))}
-    </List>
+const Solves = ({ solves, onSolveClick, showCheckboxes, activePuzzle }) => (
+  <div className="Solves">
+    {solves.length > 0 ?
+      <AppBarMargin>
+        <List>
+          {solves.map(({ duration, recordedAt, selected }) => (
+            <Solve
+              key={recordedAt}
+              recordedAt={recordedAt}
+              duration={duration}
+              selected={selected}
+              onClick={onSolveClick}
+              showCheckbox={showCheckboxes}
+            />
+          ))}
+        </List>
+      </AppBarMargin> :
+      <div className="Solves-empty">No {activePuzzle} solves</div>
+    }
     <DeletedSolveMessage />
-  </AppBarMargin>
+  </div>
 );
 
 const mapStateToProps = state => ({
   solves: getActivePuzzleSolves(state),
-  showCheckboxes: getHasActiveSelectedSolves(state)
+  showCheckboxes: getHasActiveSelectedSolves(state),
+  activePuzzle: getActivePuzzle(state)
 });
 
 const mapDispatchToProps = {
