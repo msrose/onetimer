a70767094e6d440dbfc343a1bb9a81fdbf8cbb60
Michael Rose <michael_rose@outlook.com>
2017-09-27 01:56:00 -0400

    Import debounce directly for lodash tree-shaking

--- __mocks__/lodash.js
+++
@@ -1,7 +0,0 @@
-const lodash = jest.genMockFromModule('lodash');
-
-export const debounce = x => x;
-
-lodash.debounce = debounce;
-
-export default lodash;
--- src/configure-store.js
+++ src/configure-store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { debounce } from 'lodash';
+import debounce from 'lodash/debounce';
 
 import rootReducer from './reducers';
 
