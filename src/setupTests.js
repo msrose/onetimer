e25fe9c3013318ffdb2205302bffdd94191c863a
Michael Rose <michael_rose@outlook.com>
2017-09-27 01:31:37 -0400

    Upgrade to react@16

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,11 @@
+// Make sure to load this polyfill before React
+import 'raf/polyfill';
+
+import enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+enzyme.configure({ adapter: new Adapter() });
+
 const localStorageMock = {
   getItem: jest.fn(),
   setItem: jest.fn(),
